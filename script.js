1a9dde876d1220d8989041d7e5ddccf0dd5ac80f
007hs <153376696+007hs@users.noreply.github.com>
2023-12-09 20:42:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ function calculateGearParameters() {
     const module = parseFloat(document.getElementById('module').value);
     const helixAngle = parseFloat(document.getElementById('helixAngle').value);
 
-    const pitchDiameter = module * teeth;
-    const baseDiameter = pitchDiameter * Math.cos(Math.radians(helixAngle));
-    const outerDiameter = pitchDiameter + (2 * module);
+    const pitchDiameterMM = module * teeth; // Pitch Diameter in mm
+    const baseDiameterMM = pitchDiameterMM * Math.cos(Math.radians(helixAngle)); // Base Diameter in mm
+    const outerDiameterMM = pitchDiameterMM + (2 * module); // Outer Diameter in mm
 
-    document.getElementById('pitchDiameter').innerText = `Pitch Diameter: ${pitchDiameter.toFixed(2)}`;
-    document.getElementById('baseDiameter').innerText = `Base Diameter: ${baseDiameter.toFixed(2)}`;
-    document.getElementById('outerDiameter').innerText = `Outer Diameter: ${outerDiameter.toFixed(2)}`;
+    // Convert dimensions to inches (1 inch = 25.4 mm)
+    const pitchDiameterInches = pitchDiameterMM / 25.4;
+    const baseDiameterInches = baseDiameterMM / 25.4;
+    const outerDiameterInches = outerDiameterMM / 25.4;
+
+    document.getElementById('pitchDiameter').innerText = `Pitch Diameter (mm): ${pitchDiameterMM.toFixed(2)} | (inches): ${pitchDiameterInches.toFixed(2)}`;
+    document.getElementById('baseDiameter').innerText = `Base Diameter (mm): ${baseDiameterMM.toFixed(2)} | (inches): ${baseDiameterInches.toFixed(2)}`;
+    document.getElementById('outerDiameter').innerText = `Outer Diameter (mm): ${outerDiameterMM.toFixed(2)} | (inches): ${outerDiameterInches.toFixed(2)}`;
 
     document.getElementById('results').style.display = 'block';
 }
